01539a7b8f99ee71600d75ad00f65fc24cd74326
Afif Buchori <123707908+afif-buchori@users.noreply.github.com>
2023-08-29 15:10:36 +0800

    select date

--- details/details.js
+++ details/details.js
@@ -20,3 +20,42 @@ btnCancel.addEventListener("click", () => {
   compSelect.style.display = "flex";
   formPackage.style.display = "none";
 });
+
+const btnDescAdult = document.getElementById("btn-desc-adult");
+const btnAscAdult = document.getElementById("btn-asc-adult");
+const inputAdult = document.getElementById("adult");
+btnAscAdult.addEventListener("click", () => {
+  inputAdult.value = parseInt(inputAdult.value) + 1;
+  handleCalc();
+});
+btnDescAdult.addEventListener("click", () => {
+  const value = parseInt(inputAdult.value);
+  if (value === 1) return;
+  inputAdult.value = value - 1;
+  handleCalc();
+});
+
+const btnDescChild = document.getElementById("btn-desc-child");
+const btnAscChild = document.getElementById("btn-asc-child");
+const inputChild = document.getElementById("child");
+btnAscChild.addEventListener("click", () => {
+  inputChild.value = parseInt(inputChild.value) + 1;
+  handleCalc();
+});
+btnDescChild.addEventListener("click", () => {
+  value = parseInt(inputChild.value);
+  if (value < 1) return;
+  inputChild.value = value - 1;
+  handleCalc();
+});
+
+let price = 1499512;
+const elemenPrice = document.querySelector(".total-price111");
+elemenPrice.innerHTML = "Rp.  " + price.toLocaleString("id-ID");
+const handleCalc = () => {
+  const valAdult = parseInt(inputAdult.value);
+  const valChild = parseInt(inputChild.value);
+  let totalPrice = 0;
+  totalPrice = price * valAdult + price * (valChild / 2);
+  elemenPrice.innerHTML = "Rp.  " + totalPrice.toLocaleString("id-ID");
+};
