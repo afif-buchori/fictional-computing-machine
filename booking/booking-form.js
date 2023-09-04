e49aac28e5b46b3f8d3e47fab7c3ea2a4c751931
Afif Buchori <123707908+afif-buchori@users.noreply.github.com>
2023-09-04 14:33:10 +0800

    booking form page done

--- booking/booking-form.js
+++ booking/booking-form.js
@@ -3,11 +3,15 @@ const errLastName = document.getElementById("err-ln");
 const errEmail = document.getElementById("err-email");
 const errPhone = document.getElementById("err-phone");
 const errCountry = document.getElementById("err-country");
+const errPayment = document.getElementById("err-payment");
+
 const inputFirstName = document.getElementById("first-name");
 const inputLastName = document.getElementById("last-name");
 const inputEmail = document.getElementById("email");
 const inputPhone = document.getElementById("phone");
 const inputCountry = document.getElementById("country");
+const inputRequest = document.getElementById("request");
+const paymentRadioBtn = document.getElementsByName("payment");
 
 function checkValidEmail(email) {
   var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -27,15 +31,25 @@ inputEmail.addEventListener("input", (e) => {
     if (!checkValidEmail(e.target.value)) {
       errEmail.style.display = "block";
       errEmail.innerText = "*email is invalid";
-    } else errEmail.style.display = "none";
+    } else {
+      errEmail.style.display = "none";
+    }
   } else {
     errEmail.style.display = "block";
     errEmail.innerText = "*input empty";
   }
 });
 inputPhone.addEventListener("input", (e) => {
-  if (e.target.value !== "") errPhone.style.display = "none";
-  else errPhone.style.display = "block";
+  if (e.target.value !== "") {
+    errPhone.style.display = "none";
+  } else {
+    errPhone.style.display = "block";
+    errPhone.innerText = "*input empty";
+  }
+  if (isNaN(e.target.value)) {
+    errPhone.style.display = "block";
+    errPhone.innerText = "*invalid number";
+  }
 });
 inputCountry.addEventListener("input", (e) => {
   if (e.target.value !== "") errCountry.style.display = "none";
@@ -47,8 +61,16 @@ btnSubmit.addEventListener("click", () => {
   const isValidFn = inputFirstName.value !== "";
   const isValidLn = inputLastName.value !== "";
   const isValidEmail = inputEmail.value !== "";
-  const isValidPhone = inputPhone.value !== "";
+  const isValidPhone = inputPhone.value !== "" && !isNaN(inputPhone.value);
   const isValidCountry = inputCountry.value !== "";
+
+  let paymentSelected;
+  for (const radioBtn of paymentRadioBtn) {
+    if (radioBtn.checked) {
+      paymentSelected = radioBtn.value;
+    }
+  }
+
   if (!isValidFn) errFirstname.style.display = "block";
   if (!isValidLn) errLastName.style.display = "block";
   if (isValidEmail) {
@@ -59,4 +81,30 @@ btnSubmit.addEventListener("click", () => {
   } else errEmail.style.display = "block";
   if (!isValidPhone) errPhone.style.display = "block";
   if (!isValidCountry) errCountry.style.display = "block";
+  if (!paymentSelected) errPayment.style.display = "block";
+
+  if (
+    isValidFn &&
+    isValidLn &&
+    isValidEmail &&
+    isValidPhone &&
+    isValidCountry &&
+    paymentSelected
+  ) {
+    const data = {
+      first_name: inputFirstName.value,
+      last_name: inputLastName.value,
+      email: inputEmail.value,
+      phone_number: inputPhone.value,
+      country: inputCountry.value,
+      payment_method: paymentSelected,
+      special_request: inputRequest.value,
+    };
+    console.log(data);
+  } else {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
 });
